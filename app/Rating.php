ce5e4f6c2b7c08fcb10635eb2398ec8079cc58fa
rizky redjo <celebgramme.dev@gmail.com>
2018-02-05 17:03:35 +0700

    progress icocheckr

---
+++ app/Rating.php
@@ -0,0 +1,13 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model {
+	protected $table = 'ratings';
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [ ];
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,73 +8,106 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'ICOCheckr') }}</title>
+		<link rel="shortcut icon" type="image/x-icon" href="{{ asset('/images/logo-square.png') }}">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- 
+		-->
+		<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+		<link href="{{ asset('/css/main.css') }}" rel="stylesheet">
+		
+    <!-- Scripts -->
+    <!-- 
+		-->
+		<script src="{{ asset('js/jquery-1.12.4.js') }}"></script>
+		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript">
+        $(window).on('load', function() { 
+          $("#div-loading").hide();
+        });   
+        $(document).ready(function() {
+          $(".main-content").css("min-height",$(window).height()-121);
+          $( window ).resize(function() {
+            $(".main-content").css("min-height",$(window).height()-121);
+          });
+				});
+		</script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @yield('content')
+    <div id="div-loading">
+      <div class="loadmain"></div>
+      <div class="background-load"></div>
     </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+		<nav class="navbar navbar-default navbar-static-top">
+				<div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Menu -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{ asset('images/logo-icocheckr.png') }}" alt="ICOCheckr">
+                </a>
+            </div>
+
+
+						<div class="collapse navbar-collapse" id="app-navbar-collapse" style="height:1px;">
+								<!-- Left Side Of Navbar -->
+								<ul class="nav navbar-nav">
+										&nbsp;
+								</ul>
+
+								<!-- Right Side Of Navbar -->
+								<ul class="nav navbar-nav navbar-right">
+											<li @if(Request::is('coupon')) class="active" @endif><a href="{{ url('coupon') }}">ICOCheckr</a></li>
+											<li @if(Request::is('coupon')) class="active" @endif><a href="{{ url('coupon') }}">ICO's</a></li>
+											<li @if(Request::is('coupon')) class="active" @endif><a href="{{ url('coupon') }}">Rating Guide</a></li>
+											<li @if(Request::is('coupon')) class="active" @endif><a href="{{ url('coupon') }}">FAQ</a></li>
+											<li @if(Request::is('coupon')) class="active" @endif><a href="{{ url('coupon') }}">Publish ICO</a></li>
+										
+											<li class="dropdown">
+													<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+															{{ Auth::user()->fullname }} <span class="caret"></span>
+													</a>
+
+													<ul class="dropdown-menu">
+														
+															<li>
+																	<a href="{{ route('logout') }}"
+																			onclick="event.preventDefault();
+																							 document.getElementById('logout-form').submit();">
+																			Logout
+																	</a>
+
+																	<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+																			{{ csrf_field() }}
+																	</form>
+															</li>
+													</ul>
+											</li>
+								</ul>
+						</div>
+				</div>
+		</nav>
+
+		<div @if ( (Auth::guest()) && (!Request::is('test')) ) style="background : url('{{$images_home}}') no-repeat center center fixed;  -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover,100%;" @endif class="main-content" >
+			@yield('content')
+		</div>
+				
+				
+    <div class="div-footer">
+      <p>© 2016 ICOCheckr.com All rights reserved</p>
+    </div>
+    
+
 </body>
 </html>
