6a5deacb74a93a095d046657fee5af84fa57a101
rizky redjo <celebgramme.dev@gmail.com>
2018-02-08 17:17:33 +0700

    kerangka buat ico detail

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/ico', 'IcoController@index');
 
 Route::group(['middleware' => 'auth'], function () {
 	//content writer  
