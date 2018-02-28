3ac830a52d30331caf4244de85c8debd3520afed
rizky redjo <celebgramme.dev@gmail.com>
2018-02-28 16:49:59 +0700

    fixing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,42 @@
         {
           $('#div-ico-home').html(result);
           $("#div-loading").hide();
+					
+					$('.responsive-slick').slick({
+						dots: true,
+						infinite: false,
+						speed: 300,
+						slidesToShow: 4,
+						slidesToScroll: 4,
+						responsive: [
+							{
+								breakpoint: 1024,
+								settings: {
+									slidesToShow: 3,
+									slidesToScroll: 3,
+									infinite: true,
+									dots: true
+								}
+							},
+							{
+								breakpoint: 600,
+								settings: {
+									slidesToShow: 2,
+									slidesToScroll: 2
+								}
+							},
+							{
+								breakpoint: 480,
+								settings: {
+									slidesToShow: 1,
+									slidesToScroll: 1
+								}
+							}
+							// You can unslick at a given breakpoint now by adding:
+							// settings: "unslick"
+							// instead of a settings object
+						]
+					});			
         }
       });
     }
@@ -43,6 +79,9 @@
       });
     }
 		$(document).ready(function() {
+			load_ico_home_banner();
+			load_ico_home();
+			
 			$('#select-category').append($('<option>', {value:"All",text:"All"}));
 			$('#select-category').append($('<option>', {value:"Platform",text:"Platform"}));
 			$('#select-category').append($('<option>', {value:"Cryptocurrency",text:"Cryptocurrency"}));
@@ -76,44 +115,6 @@
 			$('#select-category').append($('<option>', {value:"Travelling",text:"Travelling"}));
 			$('#select-category').append($('<option>', {value:"Skills",text:"Skills"}));
 			$('#select-category').append($('<option>', {value:"Other",text:"Other"}));
-			
-			$('.responsive-slick').slick({
-				dots: true,
-				infinite: false,
-				speed: 300,
-				slidesToShow: 4,
-				slidesToScroll: 4,
-				responsive: [
-					{
-						breakpoint: 1024,
-						settings: {
-							slidesToShow: 3,
-							slidesToScroll: 3,
-							infinite: true,
-							dots: true
-						}
-					},
-					{
-						breakpoint: 600,
-						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 2
-						}
-					},
-					{
-						breakpoint: 480,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1
-						}
-					}
-					// You can unslick at a given breakpoint now by adding:
-					// settings: "unslick"
-					// instead of a settings object
-				]
-			});			
-			load_ico_home_banner();
-			load_ico_home();
 			
 		});
 </script>
