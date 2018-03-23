b7673785f9bab719b902eee2265e5bcc59761257
rizky redjo <celebgramme.dev@gmail.com>
2018-03-23 16:48:03 +0700

    progress super slow

--- resources/views/ico/detail.blade.php
+++ resources/views/ico/detail.blade.php
@@ -16,7 +16,36 @@
 			</div>
 		</div>
 	</div>
-	
+
+	<div class="container">
+		<div class="row">
+			<!-- little menu -->
+			<div class="col-xs-12 col-md-10 pull-right navbar navbar-default navbar-static-top">
+				<ul class="nav navbar-nav">
+					<li><a href="https://icocheckr.com/article/first-step/">About</a></li>
+					<li ><a href="{{ url('/ico') }}">Trading</a></li>
+					<li ><a href="{{ url('/publish') }}">Financial</a></li>
+					<li ><a href="{{ url('/login') }}">Details</a></li>
+					<li ><a href="{{ url('/login') }}">Bookmark</a></li>
+				</ul>
+				<div>
+				</div>
+			</div>
+			
+			
+			<div class="col-xs-12 col-md-2 pull-right">
+				<div class="div-rating row">
+					Rating
+					AAA
+					<hr class="row">
+					<input type="button" class="btn" value="Buy ICO">
+				</div>
+				<div class="mini-icon row">
+				</div>
+			</div>
+		</div>
+	</div>
+
   <script>
     $(document).ready(function(){
       document.title = 'ICO rating and details';
