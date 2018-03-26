9efa50f66857e3ebab3c3125ca1677f3840b4d7e
rizky redjo <celebgramme.dev@gmail.com>
2018-03-26 09:50:59 +0700

    progress ico detail, testing video playing on website

--- resources/views/ico/detail.blade.php
+++ resources/views/ico/detail.blade.php
@@ -20,27 +20,78 @@
 	<div class="container">
 		<div class="row">
 			<!-- little menu -->
-			<div class="col-xs-12 col-md-10 pull-right navbar navbar-default navbar-static-top">
-				<ul class="nav navbar-nav">
-					<li><a href="https://icocheckr.com/article/first-step/">About</a></li>
-					<li ><a href="{{ url('/ico') }}">Trading</a></li>
-					<li ><a href="{{ url('/publish') }}">Financial</a></li>
-					<li ><a href="{{ url('/login') }}">Details</a></li>
-					<li ><a href="{{ url('/login') }}">Bookmark</a></li>
-				</ul>
-				<div>
+			<div class="col-xs-12 col-md-10 pull-right">
+				<div class="navbar navbar-default navbar-static-top navbar-detail">
+					<ul class="nav navbar-nav">
+						<li><a href="#" id="nav-about">About</a></li>
+						<li><a href="#" id="nav-trading">Trading</a></li>
+						<li><a href="#" id="nav-financial">Financial</a></li>
+						<li><a href="#" id="nav-detail">Details</a></li>
+						<li><a href="#" id="nav-about">Bookmark</a></li>
+					</ul>
+				</div>
+				<div class="main-content content-ico-about">
+					<?php echo $ico->description; ?>
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="{{$ico->url_link_video}}" allowfullscreen></iframe>
+					</div>					
+					<?php echo $ico->about; ?>
+				</div>
+				<div class="main-content content-ico-trading">
+				</div>
+				<div class="main-content content-ico-financial">
+				</div>
+				<div class="main-content content-ico-detail">
 				</div>
 			</div>
-			
-			
+
+
 			<div class="col-xs-12 col-md-2 pull-right">
 				<div class="div-rating row">
-					Rating
-					AAA
+					<div class="row">
+						<div class="col-xs-5 col-md-5">
+							<span class="label-header-rating">Rating</span>
+						</div>
+						<div class="col-xs-5 col-md-5 col-lg-offset-2">	
+							<span class="label-header-rating-value pull-right">AAA</span>
+						</div>
+					</div>
 					<hr class="row">
-					<input type="button" class="btn" value="Buy ICO">
+					<div class="row">
+						<div class="col-xs-6 col-md-6">
+							<p class="label-rating">Profile</p>
+							<p class="label-rating-value">7.0</p>
+						</div>
+						<div class="col-xs-6 col-md-6">
+							<p class="label-rating">Social</p>
+							<p class="label-rating-value">7.0</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-6 col-md-6">
+							<p class="label-rating">Team</p>
+							<p class="label-rating-value">7.0</p>
+						</div>
+						<div class="col-xs-6 col-md-6">
+							<p class="label-rating">Hype</p>
+							<p class="label-rating-value">7.0</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2">
+							<input type="button" class="btn btn-buy" value="Buy ICO">
+						</div>
+					</div>
 				</div>
 				<div class="mini-icon row">
+					<a class="icon icon-1" href="" target="_blank"></a>
+					<a class="icon icon-2" href="" target="_blank"></a>
+					<a class="icon icon-3" href="" target="_blank"></a>
+					<a class="icon icon-4" href="" target="_blank"></a>
+					<a class="icon icon-5" href="" target="_blank"></a>
+					<a class="icon icon-6" href="" target="_blank"></a>
+					<a class="icon icon-7" href="" target="_blank"></a>
+					<a class="icon icon-8" href="" target="_blank"></a>
 				</div>
 			</div>
 		</div>
@@ -49,6 +100,34 @@
   <script>
     $(document).ready(function(){
       document.title = 'ICO rating and details';
+			$(".content-ico-about").show();
+			$(".content-ico-trading").hide();
+			$(".content-ico-financial").hide();
+			$(".content-ico-detail").hide();
+			$('#nav-about').click(function(e){
+				$(".content-ico-about").show();
+				$(".content-ico-trading").hide();
+				$(".content-ico-financial").hide();
+				$(".content-ico-detail").hide();
+			});
+			$('#nav-trading').click(function(e){
+				$(".content-ico-about").hide();
+				$(".content-ico-trading").show();
+				$(".content-ico-financial").hide();
+				$(".content-ico-detail").hide();
+			});
+			$('#nav-financial').click(function(e){
+				$(".content-ico-about").hide();
+				$(".content-ico-trading").hide();
+				$(".content-ico-financial").show();
+				$(".content-ico-detail").hide();
+			});
+			$('#nav-detail').click(function(e){
+				$(".content-ico-about").hide();
+				$(".content-ico-trading").hide();
+				$(".content-ico-financial").hide();
+				$(".content-ico-detail").show();
+			});
     });
   </script>
 	
