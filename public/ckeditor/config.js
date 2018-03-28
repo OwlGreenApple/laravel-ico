f5a0633780e5fc7ca30759c53892693975a987bf
rizky redjo <celebgramme.dev@gmail.com>
2018-03-28 10:35:46 +0700

    fixing ckeditor from adding <br>

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -7,4 +7,32 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	
+CKEDITOR.on('instanceReady', function (ev) {
+
+        ev.editor.dataProcessor.writer.setRules('br',
+
+         {
+
+             indent: false,
+
+             breakBeforeOpen: false,
+
+             breakAfterOpen: false,
+
+             breakBeforeClose: false,
+
+             breakAfterClose: false
+
+         });
+
+    });
+
+ 
+
+    config.enterMode = CKEDITOR.ENTER_BR;
+
+    config.shiftEnterMode = CKEDITOR.ENTER_BR;	
 };
+
+
