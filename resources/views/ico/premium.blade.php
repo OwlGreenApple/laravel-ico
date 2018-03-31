5e78ff6901c3ea26d5338f07a52a0e5ab5ed174d
rizky redjo <celebgramme.dev@gmail.com>
2018-03-31 15:40:32 +0700

    progress premium page

--- resources/views/ico/premium.blade.php
+++ resources/views/ico/premium.blade.php
@@ -15,14 +15,18 @@
 	<div class="container main-word">
 		<div class="row">
 			<div class="col-xs-12 col-md-9 col-xs-offset-0 col-md-offset-1">
-				<h5>Targeted advertising</h5>
-				<p>Premium listing will put your ICO<br> in front of your targeted audience</p>
+				<h2>Engagement is the most effective marketing tools</h2>
+				<p>Your ICO will be exclusively announced to ICOCheckr’s potential investors. </p>
 				
-				<h5>Targeted advertising</h5>
-				<p>Your ICO will be on the top of the browse list <br>in assigned or selected categories</p>
+				<h2>Competitor is a threat NO MORE</h2>
+				<p>Higher chance for your ICO to be seen on competitors’ ICO profile, <br>while no competitor will be shown on yours.</p>
 				
-				<h5>Targeted advertising</h5>
-				<p>Your ICO will have increased visibility <br>on the competitors' ICO profiles and at the same time <br> all competitors will be removed from your ICO profile.</p>
+				<h2>Verification boosts your confidence</h2>
+				<p>Your project will be marked as a premium ICO to ensuring your project <br>as a trusted ICO.</p>
+				
+				<h2>On the top of the list</h2>
+				<p>Your ICO will be pinned on the ICOCheckr’s home page as well <br>as on our main list for the whole period, so your ICO will always be first to be seen.</p>
+				
 				<span class="word-for-free">Register your ICO for Free <br> or</span>
 				<span class="word-for-premium">Get more Benefit with <br>
 				Our ICO Registration Premium Services Now</span>
@@ -30,12 +34,112 @@
 		</div>  
 	</div>
 
+	<div class="container choose-package">
+		<div class="row">
+			<div class="col-xs-12 col-md-3 wrap-choose-package">
+				<input type="button" value="Basic" class="form-control btn header-package btn-basic">
+				
+				<div class="content-package">
+					<p class="price">3 ETH</p>
+					<hr>
+				
+					<p>3 days Featured Listing</p>
+					<p>1 Broadcast by Email</p> 
+					<p>1 Broadcast on web notification</p>
+					<p>1 Telegram channel boost</p>
+					<p>Rated by ICOCheckr</p>
+					<p>ICOCheckr’s Badge for your website</p>
+					<p>Competitor will be shown on your ICO profile</p>
+					<!--<p>Your ICO will not be shown on competitor’s profile </p>-->
+				
+				</div>
+				
+				<input type="button" value="Order Now" class="form-control btn btn-order">
+			</div>  
+			
+			<div class="col-xs-12 col-md-3 wrap-choose-package wrap-choose-package-boost">
+				<input type="button" value="Boost" class="form-control btn header-package btn-boost">
+				
+				<div class="content-package content-boost">
+					<p class="price">9 ETH</p>
+					<hr>
+				
+					<p>10 days Featured Listing</p>
+					<p>3 Broadcast by Email </p>
+					<p>3 Broadcast on web notification</p>
+					<p>3 Telegram channel boost</p>
+					<p>Rated by ICOCheckr</p>
+					<p>ICOCheckr’s Badge for your website</p>
+					<p>Competitor will not be shown on your ICO profile</p>
+					<p>Your ICO will be shown on competitor’s profile </p>
+				
+				</div>
+				
+				<input type="button" value="Order Now" class="form-control btn btn-order btn-order-boost">
+			</div>  
+			
+			<div class="col-xs-12 col-md-3 wrap-choose-package">
+				<input type="button" value="Premium" class="form-control btn header-package btn-premium">
+				
+				<div class="content-package">
+					<p class="price">20 ETH</p>
+					<hr>
+				
+					<p>45 days Featured Listing</p>
+					<p>6 Broadcast by Email </p>
+					<p>6 Broadcast on web notification</p>
+					<p>6 Telegram channel boost</p>
+					<p>Rated by ICOCheckr</p>
+					<p>ICOCheckr’s Badge for your website</p>
+					<p>Competitor will not be shown on your ICO profile</p>
+					<p>Your ICO will be shown on competitor’s profile</p>
+				
+				</div>
+				
+				<input type="button" value="Order Now" class="form-control btn btn-order">
+			</div>  
+			
+			<div class="col-xs-12 col-md-3 wrap-choose-package">
+				<input type="button" value="Platinum" class="form-control btn header-package btn-platinum">
+				
+				<div class="content-package">
+					<p class="price">30 ETH</p>
+					<hr>
+				
+					<p>60 days Featured Listing</p>
+					<p>10 Broadcast by Email </p>
+					<p>10 Broadcast on web notification</p>
+					<p>10 Telegram channel boost</p>
+					<p>Rated by ICOCheckr</p>
+					<p>ICOCheckr’s Badge for your website</p>
+					<p>Competitor will not be shown on your ICO profile</p>
+					<p>Your ICO will be shown on competitor’s profile</p>
+				
+				</div>
+				
+				<input type="button" value="Order Now" class="form-control btn btn-order">
+			</div>  
+			
+			
+		</div>  
+	</div>
 	
   <script>
+		function make_height_same(){
+			var max = -1;
+			$(".content-package").each(function() {
+					var h = $(this).height(); 
+					max = h > max ? h : max;
+			});
+			$(".content-package").each(function() {
+					$(this).height(max); 
+			});
+			$(".content-boost").height(max+60);
+		}
     $(document).ready(function(){
       document.title = 'Premium ICOCheckr Packages';
 
-			
+			make_height_same();
 			$('#button-process').click(function(e){
 			});
 			
